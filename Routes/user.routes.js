a53e5ec0e2398e6ef797cea6cf0d1428dc8ceb52
Jacox02 <jacobleandro@icloud.com>
2021-08-25 23:13:59 -0400

    Register user 6

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -13,8 +13,8 @@ router.post("/login/", (req, res) => {
   connection.end();
 });
 router.post("/signup/", (req, res) => {
-  let userName = req.body.userEmail;
-  let userEmail = req.body.userFullName;
+  let userName = req.body.userFullName;
+  let userEmail = req.body.userEmail;
   let userPassword = req.body.userPassword;
 
   let query = `call signUpUser('${userName}','${userEmail}','${userPassword}')`;
